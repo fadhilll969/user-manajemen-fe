5aa77dfd0228d30c5d7d4131fdd225b81e3994e2
fadhilll969 <sheyzz48@gmail.com>
2026-08-20 11:13:15 +0700

    Cek Fe9

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,10 +1,34 @@
 import axios from "axios";
 
-const API_URL = "https://user-manajemen-be-production.up.railway.app";
+const API_URL =
+    "https://user-manajemen-be-production.up.railway.app";
+
+// =========================
+// REGISTER
+// =========================
 
 export const createUser = async (data) => {
     const response = await axios.post(
         `${API_URL}/register`,
+        data,
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+
+    return response.data;
+};
+
+
+// =========================
+// LOGIN
+// =========================
+
+export const loginUser = async (data) => {
+    const response = await axios.post(
+        `${API_URL}/login`,
         data,
         {
             headers: {
