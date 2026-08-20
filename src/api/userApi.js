c9950234db0bfef4d8c4941a99515628da1d5357
fadhilll969 <sheyzz48@gmail.com>
2026-08-20 10:54:19 +0700

    Cek Fe6

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-kamu.up.railway.app";
+const API_URL = "https://user-manajemen-be-production.up.railway.app";
 
 export const createUser = async (data) => {
     const response = await axios.post(
